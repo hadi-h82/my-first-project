e74bc7a953af41020bc78e72451a9b028b8a06d2
Hadi <hadi.deris.h82@gmail.com>
2024-06-19 11:19:20 +0330

    edit

--- my first project/Program.cs
+++ my first project/Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Xml.Linq;
 
 namespace my_first_project
 {
